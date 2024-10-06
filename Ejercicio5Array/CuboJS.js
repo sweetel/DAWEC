64460af9612ee71fefaf1dff883134e806a05439
unknown <pablo93pablo@hotmail.com>
2024-10-06 23:21:11 +0200

    ej5

---
+++ Ejercicio5Array/CuboJS.js
@@ -0,0 +1,13 @@
+// Definimos la función cubo utilizando una arrow function con retorno implícito
+const cubo = num => num * num * num;
+
+function calcularCubo() {
+    // Obtenemos el valor ingresado por el usuario
+    const numero = document.getElementById("numero").value;
+
+    // Convertimos el valor a número y calculamos el cubo
+    const resultado = cubo(Number(numero));
+
+    // Mostramos el resultado en el párrafo correspondiente
+    document.getElementById("resultado").innerText = `El cubo de ${numero} es: ${resultado}`;
+}
